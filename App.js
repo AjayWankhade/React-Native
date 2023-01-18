93c6bf6ce4800160cb7840c0ce8bb9f821ab8187
AJAY <98700861+AjayWankhade@users.noreply.github.com>
2023-01-18 19:10:26 +0530

    Task 12- React-Native Simple Ui Done

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+import Header from './src/Header';
+import Boxes from './src/Boxes';
+export default class App extends React.Component {
+   render() {
+      return (
+         <View style={styles.container}>
+         <Header></Header>
+         <Boxes></Boxes>
+
+         </View>
+      );
+   }
+}
+
+const styles = StyleSheet.create({
+   container: {
+      flex: 1,
+   },
+});
